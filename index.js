9aca3ca14ee83c0549a3fbeb426d2da81cd9b136
developersajeeb <sajeebdeveloper@gmail.com>
2023-06-08 20:13:00 +0600

    setup mongodb successfully

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.d9amltv.mongodb.net/?retryWrites=true&w=majority";
+console.log(process.env.DB_USER);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d9amltv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
