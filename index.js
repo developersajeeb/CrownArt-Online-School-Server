5d7888547e9083c421ec1054873c2660fab52b23
developersajeeb <sajeebdeveloper@gmail.com>
2023-06-11 16:30:53 +0600

    verify jwt function ready

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if(error){
+      return res.status(403).send({error: true, massage: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
